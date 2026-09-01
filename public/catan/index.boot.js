d4fbfe39ea55953314f4e67d7daa71b8e7c6d639
MathisRouvres <mathis@myagency.group>
2026-09-01 10:52:56 +0200

    Mettre a jour HEXLAND : rotation libre et gestes sans saccade

--- public/catan/index.boot.js
+++ public/catan/index.boot.js
@@ -1,11 +1,11 @@
 
-const GODOT_CONFIG = {"args":[],"canvasResizePolicy":2,"emscriptenPoolSize":8,"ensureCrossOriginIsolationHeaders":false,"executable":"index","experimentalVK":false,"fileSizes":{"index.pck":436008,"index.wasm":39514754},"focusCanvas":true,"gdextensionLibs":[],"godotPoolSize":4,"serviceWorker":"index.service.worker.js"};
+const GODOT_CONFIG = {"args":[],"canvasResizePolicy":2,"emscriptenPoolSize":8,"ensureCrossOriginIsolationHeaders":false,"executable":"index","experimentalVK":false,"fileSizes":{"index.pck":437288,"index.wasm":39514754},"focusCanvas":true,"gdextensionLibs":[],"godotPoolSize":4,"serviceWorker":"index.service.worker.js"};
 const GODOT_THREADS_ENABLED = false;
 const engine = new Engine(GODOT_CONFIG);
 
 // Etat partage avec le jeu, lu depuis GDScript via JavaScriptBridge.
 window.hexland = {
-	version: 'f8948e6',
+	version: 'eb45966',
 	updateAvailable: false,
 	installPromptAvailable: false,
 	installOutcome: '',
--- public/catan/index.service.worker.js
+++ public/catan/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788249249|4187560|f8948e6';
+const CACHE_VERSION = '1788252699|2962287|eb45966';
 /** @type {string} */
 const CACHE_PREFIX = 'HEXLAND-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
