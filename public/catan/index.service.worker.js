fd289cc0edacfb0fda1b347894e8c44637dcb060
MathisRouvres <mathis@myagency.group>
2026-09-01 11:17:36 +0200

    Mettre a jour HEXLAND : camera sans sauts, axe vertical inverse

--- public/catan/index.service.worker.js
+++ public/catan/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788252699|2962287|eb45966';
+const CACHE_VERSION = '1788254169|4598965|2d63614';
 /** @type {string} */
 const CACHE_PREFIX = 'HEXLAND-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
